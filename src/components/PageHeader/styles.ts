90d551d4077fcadffc4eb7c9ce5eeb73e0de8056
Lucas Gomes <lucasgomesp22@gmail.com>
2020-08-06 16:06:16 -0300

    add pages and components

---
+++ src/components/PageHeader/styles.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 25,
+    paddingHorizontal: 40,
+    backgroundColor: "#8257e5"
+  },
+
+  topBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 20
+  },
+
+  title: {
+    fontFamily: 'Archivo_700Bold',
+    color: 'white',
+    fontSize: 24,
+    lineHeight: 32,
+    maxWidth: 160,
+    marginVertical: 40
+  },
+
+  logoImg: {
+    width: 45,
+    height: 45
+  }
+})
+
+export default styles
